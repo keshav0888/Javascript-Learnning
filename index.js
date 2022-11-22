37dd92a31a26128874a4aef336d2ea72f5c11dc9
keshav shinde <shindekeshav002@gmail.com>
2022-11-22 19:44:03 +0530

    code

--- index.js
+++ index.js
@@ -1,51 +1,38 @@
-/* const express = require("express");
-require("./config");
-const Product = require("./product");
-const app = express();
-app.use(express.json());
-app.post("/create", async (req, resp) => {
-  let data = new Product(req.body);
-  let result = await data.save();
-  // console.log(req.body);
-  resp.send(result);
-});
+/* console.log("keshav");
 
-app.get("/list", async (req, resp) => {
-  let data = await Product.find();
-  // console.log(req.body);
-  resp.send(data);
-});
-app.listen(3000);
+var x = 7;
+function getName() {
+  console.log("Namaste javascript");
+}
+
+getName();
+console.log(x);
+console.log("keshav shinde");
+ */
+/* var x = 1;
+a();
+b();
+function a() {
+  var x = 10;
+  console.log(x);
+}
+function b() {
+  var x = 100;
+  console.log(x);
+}
  */
 
-/* const express = require("express");
-require("./config");
-const Product = require("./product");
-const app = express();
-//app.use(express.json());
-app.get("/search/:key", async (req, resp) => {
-  console.log(req.params.key);
-  let data = await Product.find({
-    $or: [
-      {
-        name: { $regex: req.params.key },
-      },
-    ],
-  });
-  resp.send(data);
-});
-app.listen(3000); */
-const mysql = require("mysql");
-const connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  database: "student_manage",
-  password: "Keshav@888",
-});
-connection.connect((err) => {
-  if (err) {
-    console.log("error", err);
-  } else {
-    console.log("connected succuss");
+/* function a() {
+  var b = 10;
+  c();
+  function c() {
+    console.log(b);
   }
-});
+}
+a(); */
+
+{
+  var a = 10;
+  let b = 20;
+  const c = 30;
+}
--- product.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-const ProductsSchema = new mongoose.Schema({
-  name: String,
-  price: Number,
-  brand: String,
-  category: String,
-});
-module.exports = mongoose.model("products", ProductsSchema);
